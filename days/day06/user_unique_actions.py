5f37345bde7dc61f6245b619240c9349096a6aac
yeoreums <yeoreum.mail@gmail.com>
2026-01-16 12:58:13 +0900

    Day 06: user activity and unique actions

---
+++ days/day06/user_unique_actions.py
@@ -0,0 +1,31 @@
+user_actions = {}
+
+def process_event(event):
+    # event is a tuple: (user_id, action)
+    # TODO:
+    # store unique actions per user
+    user = event[0]
+    action = event[1]
+    if user not in user_actions:
+        user_actions[user] = set()
+    
+    user_actions[user].add(action)
+    
+
+def run():
+    events = [
+        ("u1", "login"),
+        ("u1", "login"),
+        ("u1", "logout"),
+        ("u2", "login"),
+        ("u2", "login"),
+        ("u2", "purchase"),
+    ]
+
+    for e in events:
+        process_event(e)
+
+    print(user_actions)
+
+if __name__ == "__main__":
+    run()
