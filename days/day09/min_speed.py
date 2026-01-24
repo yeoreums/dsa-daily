309de4c58dcfab53c67e507b77bf23245bf2e97c
yeoreums <yeoreum.mail@gmail.com>
2026-01-24 14:46:12 +0900

    Add comments

--- days/day09/min_speed.py
+++ days/day09/min_speed.py
@@ -14,10 +14,16 @@ def min_speed(piles, H):
 
     while left < right:
         mid = (left + right) // 2
+
+        # If this speed works, we try to go *slower*
+        # because we want the minimum speed that still works
         if can_finish(piles, H, mid):
             right = mid
         else:
             left = mid + 1
+
+    # left == right
+    # Smallest speed that still finishes within H hours
     return left
 
 def run():
@@ -28,3 +34,7 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+# piles totla = 27
+# 3 * 8h = 24 can't finish it
+# 4 * 8h = 32, can finish it 
